21f83589e701e2d434b8b30dd4f8feec49ca9843
bitqiang <bitqiang@outlook.com>
2018-04-13 23:28:16 +0800

    usage

--- src/components/borrowDev/children/children/borrowDevDetail.vue
+++ src/components/borrowDev/children/children/borrowDevDetail.vue
@@ -9,21 +9,21 @@
 
       <x-input title = "日期"  required v-model = "operationDetail.start_time" :readonly = "true"></x-input>
 
-      <x-input title = "用途"  required v-model = "operationDetail.usage" :readonly = "true"></x-input>
+      <x-input title = "用途"  required v-model = "operationDetail.usages" :readonly = "true"></x-input>
 
       <x-input title = "现存地点" required v-model = "operationDetail.place" :readonly = "true"></x-input>
 
       <x-input title = "负责人员" required v-model = "operationDetail.user_name" :readonly = "true"></x-input>
 
-      <x-input title = "QQ" required v-model = "operationDetail.qq_num" :readonly = "true"></x-input> 
+      <x-input title = "QQ" required v-model = "operationDetail.qq_num" :readonly = "true"></x-input>
 
-      <x-input title = "手机" required v-model = "operationDetail.phone_num" :readonly = "true"></x-input>   
+      <x-input title = "手机" required v-model = "operationDetail.phone_num" :readonly = "true"></x-input>
 
       <x-textarea title = "注意事项" :height = "30" :max = "100" v-model = "operationDetail.notice"></x-textarea>
     </group>
 
     <x-button class = "btn" type="primary"
-      @click.native = "click " 
+      @click.native = "click "
       :disabled = "type === 'checking' || type === 'waitComfirmReturn'" >{{ btn_text }}
     </x-button>
   </div>
@@ -57,21 +57,21 @@ export default {
   },
   created() {
     switch(this.type){
-      case 'borrowing':       
-	      this.title   = `借用时间，剩余${this.$route.query.surplus_day}天`; 
-        this.btn_text = '提前还'; 
+      case 'borrowing':
+	      this.title   = `借用时间，剩余${this.$route.query.surplus_day}天`;
+        this.btn_text = '提前还';
         break;
       case 'hasTimeoutReturn':
-        this.title   = '归还时间 已逾期n天'; 
-        this.btn_text = '归还'; 
+        this.title   = '归还时间 已逾期n天';
+        this.btn_text = '归还';
         break;
       case 'checking':
-        this.title   = '等待通过审核'; 
-        this.btn_text = '审核中'; 
+        this.title   = '等待通过审核';
+        this.btn_text = '审核中';
         break;
       case 'waitComfirmReturn':
-        this.title   = '已移交于借出社团'; 
-        this.btn_text = '正在途中'; 
+        this.title   = '已移交于借出社团';
+        this.btn_text = '正在途中';
         break;
     }
     this.getOperationDetail({br_id: this.br_id, type: 'in'});
